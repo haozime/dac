6d6c35ef18f2c0c1aba8ec0f91137fbd446a2139
limingv5 <limingv5@gmail.com>
2015-04-10 19:48:35 +0800

    forEach

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-exports.less = require("./engines/less");
-exports.tpl  = require("./engines/tpl");
+var fs = require("fs");
+var path = require("path");
+
+var dir = fs.readdirSync(__dirname + "/engines");
+dir.forEach(function(i) {
+  var name = path.basename(i, ".js");
+  exports[name] = require("./engines/"+name);
+});
 
 exports.iconv  = require("iconv-lite");
 exports.isUtf8 = require("is-utf8");
